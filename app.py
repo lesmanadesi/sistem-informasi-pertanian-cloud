ea819bebc27bf495f2fe27c82a8684d7cbfacd7d
lesmanadesi <lesmanadesi1919@gmail.com>
2026-01-27 20:53:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,7 @@ def tambah_lahan():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
+
