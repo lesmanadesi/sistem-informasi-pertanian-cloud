6330dee192c46e0712724aca93a84ff00eb8181c
lesmanadesi <lesmanadesi1919@gmail.com>
2026-01-27 20:19:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-from flask import Flask, render_template
-import os
-
-app = Flask(__name__)
-
-@app.route("/")
-def login():
-    return render_template("login.html")
-
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
