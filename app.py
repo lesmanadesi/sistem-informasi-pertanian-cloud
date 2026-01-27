db3b7e90c43da7c45d89410c4cc26e3ff90009bf
lesmanadesi <lesmanadesi1919@gmail.com>
2026-01-27 20:56:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect
 import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        if username == "admin" and password == "admin":
-            return redirect("/dashboard")
-        else:
-            return "Login gagal"
-
+@app.route("/")
+def home():
     return render_template("login.html")
 
+@app.route("/masuk")
+def masuk():
+    return redirect("/dashboard")
 
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
-
-@app.route("/tambah_lahan")
-def tambah_lahan():
-    return render_template("tambah_lahan.html")
-
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
-
-
