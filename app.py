1930c39db87345a38a696af5fbf26ef82d3a3db1
lesmanadesi <lesmanadesi1919@gmail.com>
2026-01-28 09:32:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,5 +70,6 @@ def logout():
 # RUN APP (LOCAL + RAILWAY)
 # ===============================
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
+
